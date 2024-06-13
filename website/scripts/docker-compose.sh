4842bee0bd79b70272866207e34ef71c8be77aaf
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-06-13 13:39:19 -0300

    feat: incluido substituição versão aplicação script docker compose

--- website/scripts/docker-compose.sh
+++ website/scripts/docker-compose.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 DIRETORIO_ENV=../.resources/environments/docker-compose.env
+DIR_POM=pom.xml
+KEY_VERSION='${WEBSITE_VERSION}'
+
+
+## Ler a versão do arquivo pom.xml e atualiza variavél de ambiente
+VALUE_VERSION=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' "$DIR_POM")
+sed -i 's,'"$KEY_VERSION"','$VALUE_VERSION',' "$DIRETORIO_ENV"
 
 mvn clean install -P build -DskipTests
 docker compose --env-file $DIRETORIO_ENV up -d --build --force-recreate
--- website/src/assets/env.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-    window.env = window.env || {};
-    // Environment variables
-    window["env"]["WEBSITE_VERSION"] = "0.0.1-SNAPSHOT";
-    window["env"]["WEBSITE_URL_APPLICATION"] = "http://localhost:4300/app";
-    window["env"]["WEBSITE_URL_GITHUB"] = "https://github.com/";
-    window["env"]["WEBSITE_URL_YOUTUBE"] = "https://www.youtube.com/";
-    window["env"]["WEBSITE_URL_LINKEDIN"] = "https://br.linkedin.com/";
-  })(this);
