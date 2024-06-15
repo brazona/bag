cf9134202e80c47a26d446aa6ff9fc93408efe6c
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-06-14 21:59:44 -0300

    feat: implementação Pipeline Deploy

---
+++ .github/workflows/scripts/prepare-manifest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##bash scripts/prepare-manifest.sh ../.resources/environments/dev.env
+
+if [ $# -eq 4 ]; then
+   DIRETORIO_ENV=$(echo $1)
+   DIR_MANIFEST_IN=$(echo $2)
+   DIR_MANIFEST_OUT=$(echo $3)
+   DOCKER_IMAGE_TAG=$(echo $4)
+else
+    echo "Parâmentro: DIRETORIO_ENV, é obrigatório";
+    echo "Parâmentro: DIR_MANIFEST_IN, é obrigatório";
+    echo "Parâmentro: DIR_MANIFEST_OUT, é obrigatório";
+    echo "Parâmentro: DOCKER_IMAGE_TAG, é obrigatório";
+    echo -e "Exemplo:\nbash .github/workflows/scripts/prepare-manifest.sh site/env.txt site/deployment.yml site/deployment-temp.yml 0.0.1-snapshot-9";
+    exit 1
+fi
+## Extrai valor do arquivo env e salva num arquivo temporário
+#echo $(cat $DIRETORIO_ENV) >> $DIRETORIO_ENV
+## Subistitui espaço por newline
+sed -i 's/\s\+/\n/g' $DIRETORIO_ENV
+find='${TAG}'
+sed -i 's,'"$find"','$DOCKER_IMAGE_TAG',' $DIRETORIO_ENV
+#sed -i 's,"$TAG",'$DOCKER_IMAGE_TAG',' $DIRETORIO_ENV
+## Gera manifesto com base no template
+rm -rf $DIR_MANIFEST_OUT
+cp  $DIR_MANIFEST_IN $DIR_MANIFEST_OUT
+## Exporta as variaveis de ambiente do arquivo temporário
+export $(grep -v '^#' $DIRETORIO_ENV | xargs) && env
+## Substitui os valores do arquivo .env para o manisfesto temporário
+envsubst < $DIR_MANIFEST_IN > $DIR_MANIFEST_OUT
+
--- website/README.md
+++ website/README.md
@@ -6,4 +6,7 @@ localhost: ``` npm run start ```
 
 dockercompose: ``` npm run docker-compose ```
 
-dockercompose: ``` kubectl apply -f deployment-run.yml ```
+kubectl_localhost: ``` kubectl apply -f deployment-run.yml ```
+
+
+bash .github/workflows/scripts/prepare-manifest.sh website/env.txt website/deployment.yml website/deployment-run.yml 0.0.1-snapshot-9
