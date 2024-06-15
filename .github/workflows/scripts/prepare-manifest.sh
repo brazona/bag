295ddb201a9254bf2f6e35dda2c74dd293562a2c
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-06-15 00:39:44 -0300

    test: run pipeline deploy

--- .github/workflows/scripts/prepare-manifest.sh
+++ .github/workflows/scripts/prepare-manifest.sh
@@ -12,20 +12,10 @@ else
     echo -e "Exemplo:\nbash .github/workflows/scripts/prepare-manifest.sh site/env.txt site/deployment.yml site/deployment-temp.yml 0.0.1-snapshot-9";
     exit 1
 fi
-echo "Paramentros"
-echo $DIRETORIO_ENV
-echo $DIR_MANIFEST_IN
-echo $DIR_MANIFEST_OUT
-echo $DOCKER_IMAGE_TAG
-echo $(cat $DIRETORIO_ENV)
-## Extrai valor do arquivo env e salva num arquivo temporário
-#echo $(cat $DIRETORIO_ENV) >> $DIRETORIO_ENV
-## Subistitui espaço por newline
+
 sed -i 's/\s\+/\n/g' $DIRETORIO_ENV
 find='_TAG_'
 sed -i 's,'"$find"','$DOCKER_IMAGE_TAG',' $DIRETORIO_ENV
-#sed -i 's,"$TAG",'$DOCKER_IMAGE_TAG',' $DIRETORIO_ENV
-## Gera manifesto com base no template
 rm -rf $DIR_MANIFEST_OUT
 cp  $DIR_MANIFEST_IN $DIR_MANIFEST_OUT
 ## Exporta as variaveis de ambiente do arquivo temporário
